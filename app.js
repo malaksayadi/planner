1352622adf8c8851bbf193c493e6218dba47a775
malaksayadi <malaksayadi04@gmail.com>
2025-04-06 12:00:43 +0100

    project initiation

---
+++ app.js
@@ -0,0 +1,14 @@
+// Import Express
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Basic route
+app.get("/", (req, res) => {
+  res.send("Hello World from Express!");
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
